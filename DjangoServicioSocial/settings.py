bf500c3c4cfc115dab32dac7c949d0f3497c7050
Pedro Contreras <fpedro23@gmail.com>
2015-06-25 17:37:18 -0500

    Prepared for deploy

--- DjangoServicioSocial/settings.py
+++ DjangoServicioSocial/settings.py
@@ -59,12 +59,24 @@ WSGI_APPLICATION = 'DjangoServicioSocial.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+# 'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbserviciosocial',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '',
+        'PORT': '',
     }
 }
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
