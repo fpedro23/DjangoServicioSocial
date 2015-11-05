27d00485c8a0a54168f4a461877688c48597baa5
Pedro Contreras <fpedro23@gmail.com>
2015-11-05 10:39:40 -0600

    Heroku Deploy

--- DjangoServicioSocial/wsgi.py
+++ DjangoServicioSocial/wsgi.py
@@ -7,10 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
-import os
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "DjangoServicioSocial.settings")
-
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn DjangoServicioSocial.wsgi:application --log-file -
+
